54f8b5c5acbcc53ba3e33de78871f2888e921b10
RicardoYoshio <rica.tomokane@gmail.com>
2023-01-16 21:34:24 -0300

    first commit

---
+++ src/Modulo9/Modulo9.java
@@ -0,0 +1,20 @@
+package Modulo9;
+
+import java.util.Scanner;
+
+public class Modulo9 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int num1 = sc.nextInt();;
+		Integer num2 = num1;
+		
+		System.out.println(num2);
+		
+
+	}
+
+}
